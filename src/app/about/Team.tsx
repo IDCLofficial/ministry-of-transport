474571338170da74309e90a91eb3749303d63524
Techxie-bc <donnaogbu@gmail.com>
2025-08-09 09:06:11 +0100

    review done

--- src/app/about/Team.tsx
+++ src/app/about/Team.tsx
@@ -8,7 +8,7 @@ const teamMembers = [
     bio: 'Provides visionary leadership and strategic direction for the Ministry of Transport in Imo State.'
   },
   {
-    name: 'Moneme Judith Fred',
+    name: 'Mrs. Moneme Judith Fred',
     role: 'Permanent Secretary',
     image: '/images/permsec.jpg',
     bio: 'Oversees administrative operations and ensures effective policy implementation.'
